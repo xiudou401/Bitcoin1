a148a0df49a80ba0053ed0316d66501dead34fcb
Will D <diweizhi@gmail.com>
2025-08-01 14:07:47 +1000

    pagination

--- src/modules/home/Pagination.tsx
+++ src/modules/home/Pagination.tsx
@@ -1,7 +1,31 @@
+'use client';
+
+import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
 import React from 'react';
 
-const Pagination = () => {
-  return <div>hah</div>;
+interface PaginationProps {
+  totalPages: number;
+}
+
+const Pagination = ({ totalPages }: PaginationProps) => {
+  const currentPage = useSearchParams().get('page') || '1';
+  console.log(currentPage);
+  return (
+    <div className="flex gap-4 text-sm font-semibold">
+      {Array.from({ length: totalPages }).map((_, index) => (
+        <Link
+          key={index}
+          href={`?page=${index + 1}`}
+          className={`${
+            currentPage === `${index + 1}` ? 'text-white' : 'text-white/50'
+          }`}
+        >
+          {index + 1}
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default Pagination;
