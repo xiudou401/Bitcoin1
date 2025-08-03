c296b801552dbd0b98581cf5fc6f98eae4d6b008
Will D <diweizhi@gmail.com>
2025-08-03 22:22:06 +1000

    post

---
+++ src/app/post/[id]/page.tsx
@@ -0,0 +1,13 @@
+import Layout from '@/components/Layout';
+import Content from '@/modules/post/Content';
+import React from 'react';
+
+const Post = () => {
+  return (
+    <Layout>
+      <Content />
+    </Layout>
+  );
+};
+
+export default Post;
