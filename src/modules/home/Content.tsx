01f933ebe2b9728d3f63919e6d66beb8988ec5f4
Will D <diweizhi@gmail.com>
2025-08-03 14:17:01 +1000

    comment editor

---
+++ src/modules/home/Content.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+import React from 'react';
+import CommentBtn from './CommentBtn';
+
+const Content = () => {
+  return (
+    <>
+      <Image
+        src="/images/bitcoin-banner.jpeg"
+        alt="bitcoin-banner"
+        className="w-full rounded-lg border border-white/10"
+        width={1584}
+        height={396}
+      />
+      <h1 className="text-2xl font-bold mt-2">@bitcoin</h1>
+      <p className='text-sm text-white/50 mt-2"'>
+        Bitcoin is a decentralized digital currency that enables instant,
+        peer-to-peer transactions without intermediaries. It is based on
+        blockchain technology, which is a distributed ledger that records
+        transactions in a secure and transparent manner.
+      </p>
+      <div className="w-full mt-8">
+        <CommentBtn />
+      </div>
+    </>
+  );
+};
+
+export default Content;
---
+++ src/modules/post/content.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import Post from '@/components/Post';
+import { useRouter } from 'next/navigation';
+
+import React from 'react';
+
+const mockPost: Post = {
+  id: '1',
+  title: 'Post 1',
+  content: 'Content 1',
+  createdAt: 0,
+};
+
+const Content = () => {
+  const router = useRouter();
+  return (
+    <div>
+      <button
+        onClick={() => {
+          router.back();
+        }}
+        className="text-sm text-white font-bold"
+      >
+        {'← Back'}
+      </button>
+      <Post post={mockPost} />
+    </div>
+  );
+};
+
+export default Content;
